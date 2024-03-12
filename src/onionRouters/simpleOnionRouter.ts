9a7de39d17478be4b446cc42c87d7cd1e1178767
clementcaron <95753473+clementcaron@users.noreply.github.com>
2024-03-12 11:58:52 +0100

    Update simpleOnionRouter.ts

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -2,33 +2,38 @@ import bodyParser from "body-parser";
 import express from "express";
 import { BASE_ONION_ROUTER_PORT } from "../config";
 
+// Variables to store messages states
+let lastReceivedEncryptedMessage = null;
+let lastReceivedDecryptedMessage = null;
+let lastMessageDestination = null;
+
 export async function simpleOnionRouter(nodeId: number) {
   const onionRouter = express();
   onionRouter.use(express.json());
   onionRouter.use(bodyParser.json());
 
-  let lastReceivedEncryptedMessage = null;
-  let lastReceivedDecryptedMessage = null;
-  let lastMessageDestination = null;
-
-  // Implement the status route
   onionRouter.get("/status", (req, res) => {
     res.send('live');
   });
 
-  // GET route for the last received encrypted message
+  // Route for the last received encrypted message
   onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
     res.json({ result: lastReceivedEncryptedMessage });
   });
 
-  // GET route for the last received decrypted message
+  // Route for the last received decrypted message
   onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
     res.json({ result: lastReceivedDecryptedMessage });
   });
 
-  // GET route for the last message destination
+  // Route for the last message's destination
   onionRouter.get("/getLastMessageDestination", (req, res) => {
     res.json({ result: lastMessageDestination });
   });
 
-  const server = onionRouter.listen(BASE_ONION_ROUTER_PO
+  const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
+    console.log(`Onion router ${nodeId} is listening on port ${BASE_ONION_ROUTER_PORT + nodeId}`);
+  });
+
+  return server;
+}
