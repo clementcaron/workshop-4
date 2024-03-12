8ecc358244c6601e9892aa2b425ab812c1f6e12a
clementcaron <95753473+clementcaron@users.noreply.github.com>
2024-03-12 11:59:12 +0100

    Update user.ts

--- src/users/user.ts
+++ src/users/user.ts
@@ -2,25 +2,31 @@ import bodyParser from "body-parser";
 import express from "express";
 import { BASE_USER_PORT } from "../config";
 
-export type SendMessageBody = {
-  message: string;
-  destinationUserId: number;
-};
+// Variables to store messages states
+let lastReceivedMessage = null;
+let lastSentMessage = null;
 
 export async function user(userId: number) {
   const _user = express();
   _user.use(express.json());
   _user.use(bodyParser.json());
 
-  // Implement the status route
   _user.get("/status", (req, res) => {
     res.send('live');
   });
 
+  // Route for the last received message
+  _user.get("/getLastReceivedMessage", (req, res) => {
+    res.json({ result: lastReceivedMessage });
+  });
+
+  // Route for the last sent message
+  _user.get("/getLastSentMessage", (req, res) => {
+    res.json({ result: lastSentMessage });
+  });
+
   const server = _user.listen(BASE_USER_PORT + userId, () => {
-    console.log(
-      `User ${userId} is listening on port ${BASE_USER_PORT + userId}`
-    );
+    console.log(`User ${userId} is listening on port ${BASE_USER_PORT + userId}`);
   });
 
   return server;
